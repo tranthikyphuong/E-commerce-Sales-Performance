8a3f2825ade9c2c4707ca7e8e537823e3d7026c0
tranthikyphuong <ttkphuong.9d@gmail.com>
2025-03-24 14:28:30 +0700

    update 1.

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,1 +1,5 @@
-print('hello world!')
+import numpy as np 
+import pandas as pd 
+import csv
+df1=pd.read_csv('C:/Users/PC KY PHUONG/OneDrive/Documents/Project SQL/Data_/E-commerece sales data 2024.csv')
+print(df1)
