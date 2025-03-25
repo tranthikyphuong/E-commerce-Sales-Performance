46541ea80d27099aae88ef6b0459bb0dc9501207
tranthikyphuong <ttkphuong.9d@gmail.com>
2025-03-25 16:28:36 +0700

    update 3.0

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,9 +3,6 @@ import pandas as pd
 import csv
 from scipy import stats
 import seaborn as sns
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 
 def detect_outliers_zscore(data,columns):
@@ -55,35 +52,6 @@ print("Customer id check duplicate: ",customerDetails['Customer ID'].duplicated(
 numeric_columns = customerDetails.select_dtypes(include=['number']).columns
 detect_outliers_zscore(customerDetails,numeric_columns)
 print(customerDetails)
-# Predict value
-mergedData = customerDetails.merge(productDetails[['subcategory']], left_on='Category', right_on='subcategory', how='left')
-mergedData['Category'] = mergedData['Category'].fillna('Unknown')
-mergedData['subcategory'].fillna("Unknown", inplace=True)
-le_category = LabelEncoder()
-mergedData['category_encoded'] = le_category.fit_transform(mergedData['Category'])
-
-train_data = mergedData.dropna(subset=['subcategory'])
-test_data = mergedData[mergedData['subcategory'].isna()]
-
-X_train = train_data[['category_encoded']]
-y_train = train_data['subcategory']
-if X_train.empty or y_train.empty:
-    print("Dữ liệu train bị rỗng!")
-else:
-    print("Khong rỗng!")
-
-X_test = test_data[['category_encoded']]
-
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
-
-predicted_subcategories = model.predict(X_test)
-mergedData.loc[mergedData['subcategory'].isna(), 'subcategory'] = predicted_subcategories
-
-productDetails['subcategory'] = mergedData['subcategory']
-print(productDetails)
-
-
 
 plt.figure(figsize = (12,6))
 sns.boxplot(data=customerDetails[numeric_columns])
